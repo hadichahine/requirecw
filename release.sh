89df8d98a2f6b2fa60229e69aa11eed05d358d6a
Hadi Chahine <hadichahine232@hotmail.com>
2018-10-20 23:41:31 +0300

    Modify main script on release.

--- release.sh
+++ release.sh
@@ -3,6 +3,6 @@
 mkdir release
 cp source/* release/
 cp package.json release/
-cd release
 node_modules/.bin/json -I -f release/package.json -e 'this.scripts = undefined'
 node_modules/.bin/json -I -f release/package.json -e 'this.devDependencies = undefined'
+node_modules/.bin/json -I -f release/package.json -e "this.main = 'requirecw.js'"
