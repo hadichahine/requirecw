159da648e9d575e306025705263297461b03fdaa
Hadi Chahine <hadichahine232@hotmail.com>
2018-10-20 23:38:27 +0300

    Add remove scripts and devDependencies from release/package.json.

--- release.sh
+++ release.sh
@@ -2,5 +2,7 @@
 
 mkdir release
 cp source/* release/
+cp package.json release/
 cd release
-npm init
+node_modules/.bin/json -I -f release/package.json -e 'this.scripts = undefined'
+node_modules/.bin/json -I -f release/package.json -e 'this.devDependencies = undefined'
